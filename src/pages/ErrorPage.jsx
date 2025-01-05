ebf4ba075378147d5ff26a2e0f82bc0a31c71a6f
User <mahiibnaufal@gmail.com>
2025-01-05 19:23:07 +0700

    first commit

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+  return (
+    <>
+      <h1 className="flex w-full h-screen items-center justify-center">
+        HALAMAN TIDAK ADA 404
+      </h1>
+    </>
+  );
+};
+
+export default ErrorPage;
